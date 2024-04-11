aa196f07757a2303535b32b64538b89aed351fda
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-04-11 10:13:52 +0630

    E781 key change

--- config/game.php
+++ config/game.php
@@ -1,12 +1,12 @@
 <?php 
 return [
     'api' => [
-        'operator_code' => 'E780',
+        'operator_code' => 'E781',
         // 'password'  => 'abcd1234',
         // 'secret_key' => 'ljfVJA',
         // 'url' => 'https://swmd.6633663.com',
         'password'  => 'abcd1234',
-        'secret_key' => 'cAUCKV',
+        'secret_key' => 'j57oAy',
         'url' => 'https://swmd.6633663.com',
     ],
 ];
